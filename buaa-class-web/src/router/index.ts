8535bc24cad37aef0b6e84e5fba990264638e5b2
jigggger <2387315951@qq.com>
2023-04-24 00:39:27 +0800

    login

--- buaa-class-web/src/router/index.ts
+++ buaa-class-web/src/router/index.ts
@@ -1,28 +1,3 @@
-// import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
-// import login from "../views/login/index.vue"
-// const routes: Array<RouteRecordRaw> = [
-//   {
-//     path: '/',
-//     name: 'home',
-//     component: HomeView
-//   },
-//   {
-//     path: '/about',
-//     name: 'about',
-//     // route level code-splitting
-//     // this generates a separate chunk (about.[hash].js) for this route
-//     // which is lazy-loaded when the route is visited.
-//     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-//   }
-// ]
-
-// const router = createRouter({
-//   history: createWebHistory(process.env.BASE_URL),
-//   routes
-// })
-
-// export default router
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 
@@ -31,6 +6,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'Home',
     component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/LoginRegister.vue')
   },
 ]
 
