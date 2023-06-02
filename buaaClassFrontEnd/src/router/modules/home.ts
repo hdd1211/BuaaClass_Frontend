16aa8a30bb50d0cda0a1b9734c786a45c44d3607
aaa <aaa>
2023-06-02 19:24:32 +0800

    route1

--- buaaClassFrontEnd/src/router/modules/home.ts
+++ buaaClassFrontEnd/src/router/modules/home.ts
@@ -1,9 +1,24 @@
 import { DashboardIcon } from 'tdesign-icons-vue-next';
 import { shallowRef } from 'vue';
-
+import { LogoutIcon } from 'tdesign-icons-vue-next';
 import Layout from '@/layouts/index.vue';
 
 export default [
+  {
+    path: '/loginRedirect',
+    name: 'loginRedirect',
+    redirect: '/login',
+    meta: { title: '登录页', icon: shallowRef(LogoutIcon) },
+    component: () => import('@/layouts/blank.vue'),
+    children: [
+      {
+        path: 'index',
+        redirect: '/login',
+        component: () => import('@/layouts/blank.vue'),
+        meta: { title: '登录中心' },
+      },
+    ],
+  },
   {
     path: '/home',
     component: Layout,
